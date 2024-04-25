ab33857d66285e1cbcca1b38e2d15db7acc2e14e
Bagus Nugraha <167946759+bagusnugraha7@users.noreply.github.com>
2024-04-25 19:17:32 +0700

    Commit dari lokal
    pelatihan hari kedua

---
+++ main.js
@@ -0,0 +1,52 @@
+const PI = 3.14
+
+let angka = 5
+
+//console.log(PI)
+//console.log(angka)
+
+angka = 7
+
+//PI = 4
+
+//console.log(angka)
+
+let nama = "Bagus"
+
+//console.log(nama)
+
+let perempuan = false
+
+//console.log(perempuan)
+
+//console.log(typeof(PI))
+//console.log(typeof(angka))
+//console.log(typeof(nama))
+//console.log(typeof(perempuan))
+
+// let data = null
+
+// console.log(data)
+// console.log(typeof(data))
+
+// let variable
+
+// console.log(variable)
+// console.log(typeof(variable))
+
+// // FUNGSI
+// function f(x){
+//     return 5 + x
+// }
+
+// f(5)
+
+// console.log(f(5))
+// console.log(f(7))
+
+// function f(x,y){
+//     return 10/x+y
+// }
+
+// console.log(f(2,3))
+// console.log(f(5,3))
